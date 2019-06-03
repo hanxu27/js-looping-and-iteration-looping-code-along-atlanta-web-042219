edb81c5e580769d02fa3e759ec57c292db40b4d4
Han Xu <hanxu27@gmail.com>
2019-06-02 20:02:23 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function writeCards(params) {
+  const output = [];
+  for (let i = 0; i < params.length; i++) {
+    output[i] = `Thank you, ${params[i]}, for the wonderful surprise gift!`;
+  }
+  return output;
+}
+
+function countdown(number) {
+  for (let index = number; index >= 0; index--) {
+    console.log(index);
+  }
+}
